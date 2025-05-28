da4165f5a7c4a8ade2fab6466a6bdaab37071ffa
Willian Luis Grzybowski de Lima <williangrzybowski@gmail.com>
2025-05-28 07:58:11 -0300

    modif user page, add colecao and others elements

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,4 +1,28 @@
 //Função para fazer o dropdown da postagem funcionar 
+
+
+const menuList = document.getElementById('navlinks')
+const overlay = document.getElementById('overlay')
+const btnMenuList = document.getElementById('hamburger')
+const profileBtn = document.getElementById('dropdown-perfil')
+const dropdownMenu = document.getElementById('dropdownMenu')
+
+
+btnMenuList.addEventListener('click', () => {
+  menuList.classList.toggle('open')
+  hamburger.classList.toggle('open')
+  overlay.classList.toggle('show')
+})
+
+overlay.addEventListener('click', () => {
+  menuList.classList.remove('open')
+  hamburger.classList.remove('open')
+  overlay.classList.remove('show')
+  profileMenu.classList.remove('open')
+  dropdownMenu.classList.remove('show')
+});
+
+
 function toggleOptions(element) {
     const container = element.parentElement;
     container.classList.toggle('active');
@@ -11,15 +35,11 @@ document.addEventListener('click', function(e) {
       }
   });
 });
-  
 
-function toggleMenu(elemento, icone) {
-    var menu = document.getElementById(elemento);
-    var icone = document.getElementById(icone)
-    if (menu.style.display === "flex") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "flex";
+profileBtn.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('show')
+  overlay.classList.toggle('show')
+})
 
-    }
-}
+
+
