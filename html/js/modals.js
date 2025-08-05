33947a23236c3618f489b3e61b2b2ade5f6b2552
Willian Luis Grzybowski de Lima <williangrzybowski@gmail.com>
2025-08-05 11:28:56 -0300

    modif modal post

--- html/js/modals.js
+++ html/js/modals.js
@@ -1,12 +1,15 @@
   const modalPost = document.getElementById("modalPost");
-  const openBtn = document.querySelector(".btnPost");
+  const openBtn = document.querySelector(".foto");
+  const overlay2 = document.getElementById('overlay2')
   const closeBtn = modalPost.querySelector(".sair");
 
   openBtn.addEventListener("click", () => {
     modalPost.style.display = "flex"; 
+    overlay2.classList.toggle('show')
   });
 
   closeBtn.addEventListener("click", (e) => {
     e.preventDefault(); 
     modalPost.style.display = "none"; 
+    overlay2.classList.remove('show')
   });
