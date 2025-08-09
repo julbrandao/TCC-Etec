1b8fcc086440ed99de1ada4adc4177bf44cb6fda
Willian Luis Grzybowski De Lima <williangrzybowski@gmail.com>
2025-08-09 17:44:28 -0300

    corrigindo erros

--- Downloads/TCC-Etec-main/TCC-Etec-main/app.js
+++
@@ -1,8 +0,0 @@
-// Importações:
-
-// npm init -y 
-// npm install express sequelize mysql2 ejs ejs-layouts	 
-// npm install --save-dev nodemon 
-
-//add logica da page de cadastro, e page base para add a navbar.
-//mover arquivos de cadastro e login para a pasta que desejar
