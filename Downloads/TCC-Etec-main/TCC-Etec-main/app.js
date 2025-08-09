8f378737422cd9ee215fee87300ddf2f66944d99
Willian Luis Grzybowski De Lima <williangrzybowski@gmail.com>
2025-08-09 17:30:28 -0300

    modif navbar, sign up pages

---
+++ Downloads/TCC-Etec-main/TCC-Etec-main/app.js
@@ -0,0 +1,8 @@
+// Importações:
+
+// npm init -y 
+// npm install express sequelize mysql2 ejs ejs-layouts	 
+// npm install --save-dev nodemon 
+
+//add logica da page de cadastro, e page base para add a navbar.
+//mover arquivos de cadastro e login para a pasta que desejar
